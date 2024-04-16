91f3517ede8a4b448abc68bc598ffc4ca60eba5b
thiagosmacedo <thiago.gigacomp@gmail.com>
2024-04-16 10:03:24 -0300

    atualizado

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ let key = "5abdc12b187554a802458325d544fdeb"
 function mostrarTela(dados) {
     console.log(dados)
     document.querySelector(".city").innerHTML = " Tempo em " + dados.name
-    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".temp").innerHTML = "Agora " + Math.floor(dados.main.temp) + "°C"
     document.querySelector(".type").innerHTML = dados.weather[0].description
     document.querySelector(".humidity").innerHTML = "Umidade:  " + dados.main.humidity + "%"
     document.querySelector(".img-logo").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png` 
